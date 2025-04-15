80ed580ff15d160b899862deb6175f486040b245
payjuper <yj1son@naver.com>
2025-04-15 12:13:48 +0900

    prompt fix

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ deployment_name = os.getenv("AZURE_OPENAI_DEPLOYMENT_ID")
 def get_random_hero():
     prompt = (
         "한국사에 등장하는 위인 중에서 무작위로 한 명을 선정해서 소개해줘.\n"
+        "가능하면 이전에 자주 등장하지 않았던 인물로 해주세요.\n"
         "다음 형식으로 작성해줘:\n\n"
         "[이름](출생연도~사망연도)\n"
         "핵심업적: [대표 업적 한 줄]"
